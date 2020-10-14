9dcee0c6175df1b224ec96ae3594b525aa009752
Andrew102938 <hello99@DESKTOP-M2REPTJ.localdomain>
2020-10-14 18:25:48 -0400

    Inital impl for absolute.py

---
+++ absolute.py
@@ -0,0 +1,20 @@
+import random
+
+while True:
+    randomnum = range(-100, 101)
+    randomnum2 = random.choice(randomnum)
+    expected = abs(randomnum2) * 2
+    while True:
+        actual = input(f'Enter the absolute value of {randomnum2} times two: ')
+        if actual.isnumeric():
+            break
+        else:
+            continue
+    while True:
+        actual = input(f'Enter the absolute value of {randomnum2} times two: ')
+        if int(actual) == expected:
+            print('You are correct')
+            break
+        else:
+            print('Try again')
+            continue
