f3b879ea1afdae4f23a8ed17182568f0ce67bc5a
Andrew Fang <welcomeandrewfang@gmail.com>
2022-07-17 14:12:20 -0400

    debugged

--- drawrectangle.py
+++ drawrectangle.py
@@ -1,9 +1,10 @@
 l = int(input("Enter your length here: "))
 w = int(input("Enter your width here: "))
+char = str(input("Enter your character here: "))
 for x in range(0, l):
     for y in range(0, w):
         if x == 0 or x == l-1 or y == 0 or y == w-1:
-            print(end="* ")
+            print(end=char+" ")
         else:
             print(end="  ")
 
