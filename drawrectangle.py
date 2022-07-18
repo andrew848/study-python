fb1595e5392747251fe0021d92a50814068a6c8d
Andrew Fang <welcomeandrewfang@gmail.com>
2022-07-18 12:11:18 -0400

    debugged

--- drawrectangle.py
+++ drawrectangle.py
@@ -1,10 +1,23 @@
-l = int(input("Enter your length here: "))
-w = int(input("Enter your width here: "))
+def isPositiveNumber(num):
+    if not num.isdigit():
+        print("only positive numbers")
+        return 1
+    elif int(num) < 0:
+        print("only positive numbers")
+        return 1
+    return int(num)
+
+
+l = isPositiveNumber(input("Enter your length here: "))
+w = isPositiveNumber(input("Enter your width here: "))
+
 char = str(input("Enter your character here: "))
-for x in range(0, l):
-    for y in range(0, w):
-        if x == 0 or x == l-1 or y == 0 or y == w-1:
-            print(end=char+" ")
+for x in range(0, int(l)):
+    for y in range(0, int(w)):
+        if int(l) < 0:
+            print('enter only positive numbers')
+        if x == 0 or x == int(l) - 1 or y == 0 or y == int(w) - 1:
+            print(end=char + " ")
         else:
             print(end="  ")
 
