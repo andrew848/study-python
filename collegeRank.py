a8b0252ace34593c4c9ce78dc76d8a85c5e0c758
Andrew Fang <welcomeandrewfang@gmail.com>
2022-08-28 11:54:19 -0400

    wrote a function that shows the rank of a college

---
+++ collegeRank.py
@@ -0,0 +1,17 @@
+colleges = {'princeton': 1, 'harvard': 2, 'mit': 3, 'yale': 4, 'stanford': 5, 'uc': 6, 'up': 7, 'cit': 8, 'duke': 9,
+            'jhu': 10, 'nu': 11, 'darmouth': 12, 'brown': 13, 'vanderbilt': 14, 'wu': 15, 'cornell': 16, 'rice': 17,
+            'notre dame': 18, 'ucle': 19, 'emory': 20, 'uc berkeley ': 21, 'georgetown': 22, 'um ann abror': 23,
+            'carnegie': 24,
+            'uv': 25, 'usc': 26, 'nyu': 27, 'tufts': 28, 'uc santa barbara': 29, 'uf': 30}
+
+
+def rankCollege(college):
+    lower = college.lower()
+    if college not in colleges:
+        msg = college + ' is out of range'
+    else:
+        msg = college + ' is ranked ' + str(colleges[lower])
+    return msg
+
+
+print(rankCollege('harvard'))
